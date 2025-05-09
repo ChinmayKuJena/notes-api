8429de92319052b8a01917c8353757640fe4bbb1
SR <103866433+SRobver@users.noreply.github.com>
2025-05-09 19:10:28 +0530

    Update notes.service.ts i.e added the dataui/crud and appropriate injectables

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,4 +1,13 @@
+import { TypeOrmCrudService } from '@dataui/crud-typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { NotesEntity } from 'src/_core/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class NotesService {}
+export class  NotesService extends TypeOrmCrudService<UserEntity> {
+    constructor(@InjectRepository(NotesEntity) private _repo: Repository<NotesEntity>) {
+        super(_repo);
+    }
+}
+
