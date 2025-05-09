e86248fcbd04ff4b2a5029e4bd7cb977b75e0380
SR <103866433+SRobver@users.noreply.github.com>
2025-05-09 16:30:06 +0530

    Update notes.controller.ts with dataui/crud

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,6 +1,21 @@
+
+
 import { Controller } from '@nestjs/common';
 import { NotesService } from './notes.service';
-
+import { Crud, CrudController } from '@dataui/crud';
+import { NotesEntity } from 'src/_core/entities/notes.entity';
+@Crud({
+  model: {
+    type: NotesEntity,
+  },
+  params: {
+    user_id: {
+      field: 'user_id',
+      primary: true,
+      type: 'number',
+    },
+  },
+})
 @Controller('notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
