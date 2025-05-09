bc4b138304dc5a4e124bd08af5004205490d41f8
SR <103866433+SRobver@users.noreply.github.com>
2025-05-09 19:26:13 +0530

    Update notes.module.ts  added TypeOrmModule

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { NotesService } from './notes.service';
 import { NotesController } from './notes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotesEntity } from 'src/_core/entities/user.entity';
+
 
 @Module({
+  imports:[TypeOrmModule.forFeature([NotesEntity])],
   controllers: [NotesController],
   providers: [NotesService],
 })
